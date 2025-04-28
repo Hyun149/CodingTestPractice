45ce264534476c1d4461af065273099298e4170f
Hyun149 <rusia4527@Gmail.com>
2025-04-28 13:55:31 +0900

    ✨ 코딩테스트 연습: 배열 최솟값과 인덱스 찾기 문제 풀이 완료
    ✨ 코딩테스트 연습: 배열 최솟값과 인덱스 찾기 문제 풀이 완료

---
+++ CodingTestPractice/CodingTestPractice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 42, 17, 88, 59, 34, 60 };
+        int min = numbers[0];
+        int index = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] < min) 
+            {
+                min = numbers[i];
+                index = i;
+            }
+        }
+
+        Console.WriteLine(min);
+        Console.WriteLine(index + 1);
+    }
+}
