162effa28347e27baa7de0b7ca792361533948e6
Hyun149 <rusia4527@Gmail.com>
2025-04-29 09:19:17 +0900

    feat(day2): 1부터 n까지 3 또는 5의 배수 합 구하기 문제 풀이
    feat(day2): 1부터 n까지 3 또는 5의 배수 합 구하기 문제 풀이

---
+++ CodingTestPractice/CodingTestPractice/Day 2/SumMultiples.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+class Program
+{
+    // 📝 문제:
+    // 1부터 n까지의 수 중에서
+    // 3의 배수 또는 5의 배수인 수를 모두 찾아
+    // 그 수들의 합을 구하여 출력하는 프로그램입니다.
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int sum = 0;
+
+        for (int i = 0; i <= n; i++)
+        {
+            if (i % 3 == 0 || i % 5 == 0)
+            {
+                sum += i;
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+}
